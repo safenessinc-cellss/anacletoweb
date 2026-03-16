4c246e4387de3edc16d6576553dabe87d6ec5a85
safenessinc-cellss <safeness.inc@gmail.com>
2026-03-16 14:10:25 -0300

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Backend funcionando'));
-app.listen(port, () => console.log(`Servidor en puerto ${port}`));
+app.get('/', (req, res) => res.send('Backend funcionando!'));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
